94999aeea9f15c386276153ba35c31f26057d512
jose-ewerton <jose.ewerton@escolar.ifrn.edu.br>
2022-01-10 08:20:44 -0300

    static files

--- projeto/settings.py
+++ projeto/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
